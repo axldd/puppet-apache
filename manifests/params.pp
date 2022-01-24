05cb0be54c679b008ab331c614a21cb7b43c32db
root <root@92a28ef4181c.mylabserver.com>
2022-01-24 15:20:45 +0000

    refactore install class

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+# @summary 
+#
+# OS related vars 
+#
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
